21f2d0b5fb5e20e927354d9cb174413dd93f41bd
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-05 21:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	const charList = ['a','b','c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	let result = "";
+	for (let i = 0; i < l; i++) {
+		result += charList[Math.floor(Math.random()*charList.length)];
+	}
+	return result;
 }
 
 // Do not change the code below.
